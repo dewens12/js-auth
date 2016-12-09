9883ff83043ba31579fe24d43f885dd7cfa928c3
Luke Elmers <lukeelmers@gmail.com>
2016-12-08 16:39:13 -0800

    chore: Update test scripts.

--- test/setup.js
+++ test/setup.js
@@ -9,4 +9,5 @@ require('babel-register')();
 var jsdom = require('jsdom');
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
+global.location = window.location || {};
 global.navigator = global.window.navigator;
