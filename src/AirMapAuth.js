c23ff75ad1f0b7bf8b431a8a545649bac2326fcb
Luke Elmers <lukeelmers@gmail.com>
2017-06-27 15:13:40 -0700

    fix: Update email verification error message.

--- src/AirMapAuth.js
+++ src/AirMapAuth.js
@@ -1,7 +1,7 @@
 const Auth0Lock = require('auth0-lock').default;
 const jwt = require('jsonwebtoken');
-const { BadConfigError } = require('./error');
-const { supportsLocalStorage, getErrorMessage } = require('./utilities.js');
+const { AuthorizationError, BadConfigError } = require('./error');
+const { supportsLocalStorage } = require('./utilities.js');
 
 /** Class for handling the AirMap Auth Module */
 class AirMapAuth {
@@ -82,7 +82,7 @@ class AirMapAuth {
             window.alert('Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Please try exiting Private Browsing Mode and logging in again, or using another browser.');
         }
         // Auth0 Lock Event Emitters
-        // Listens to the 'authenticated' event which is emitted when a user logs in and emmediately stores a token into localStorage.
+        // Listens to the 'authenticated' event which is emitted when a user logs in and immediately stores a token in localStorage.
         this._lock.on('authenticated', (authResult) => {
             localStorage.setItem(this._tokenName, authResult.idToken);
             this._userId = authResult.idTokenPayload.sub;
@@ -95,14 +95,16 @@ class AirMapAuth {
         });
         // Listens to 'authorization_error' which is emitted when authorization fails. Calls logout without a redirect, launches an Auth Modal, and parses error for user.
         this._lock.on('authorization_error', (error) => {
-            const errorMsg = getErrorMessage(error);
             this.logout();
-            if (!errorMsg) {
-                console.warn(error);
-                this._lock.show({ flashMessage: { type: 'error', text: 'There was an error authenticating.' } });
-            } else {
-                this._lock.show({ flashMessage: { type: 'error', text: errorMsg } });
+            const err = {
+                ...error,
+                error_description: {
+                    type: '',
+                    ...JSON.parse(error.error_description)
+                }
             }
+            const authErr = new AuthorizationError(err.error_description.type);
+            this._showAuthError(authErr.getText());
             this.opts.onAuthorizationError(error);
         });
         // Attaching event listener for DOM load when autoLaunch is desired so that an authenticated check is made.
@@ -114,12 +116,27 @@ class AirMapAuth {
     }
 
     /**
+     *  Launches the Auth Modal with an error message displayed.
+     *  @private
+     *  @param {string} text - Error message text to display.
+     *  @return {void}
+     */
+    _showAuthError(text) {
+        this._lock.show({
+            flashMessage: {
+                type: 'error',
+                text
+            }
+        });
+    }
+
+    /**
      *  Launches the Auth Modal after checking if a valid auth token is available.
      *  @public
      *  @return {void}
      */
     showAuth() {
-        //Will only show Auth Modal when user does not have a valid auth token available.
+        // Will only show Auth Modal when user does not have a valid auth token available.
         // Also, handling race conditions by checking hash for id_token as a redirect (causing DOM loading) fires before 'authenticated' event.
         let authenticated = this.isAuthenticated();
         if (authenticated || window.location.hash.indexOf('id_token') > -1) {
--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,3 @@
-const escape = require('lodash.escape');
-
 const supportsLocalStorage = () => {
     // Safari, in Private Browsing Mode, looks like it supports localStorage but all calls to setItem
     // throw QuotaExceededError. We're going to detect this and just silently drop any calls to setItem
@@ -21,28 +19,7 @@ const supportsLocalStorage = () => {
     }
 }
 
-const getErrorMessage = (error) => {
-    const err = JSON.parse(error.error_description);
-    switch (err.type) {
-        case 'domain_blacklist':
-            return 'Sorry, we are unable to create an account for this email address. Please register with an address from another domain.';
-            break;
-        case 'email_verification':
-            return err.resend_link ?
-                `Your email address has not been verified. Please check the link that was emailed to you, or <a style="color: #fff; text-decoration: underline;" href="${escape(err.resend_link)}">click here</a> to resend the link.` :
-                `Your email address has not been verified. Please check the link that was emailed to you.`;
-            break;
-        default:
-            break;
-    }
-}
-
-const validateLink = (str) => {
-    let regex = /https?:\/\/api.airmap\.io(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?/;
-    return !!str.match(regex);
-}
 
 module.exports = {
-    getErrorMessage,
     supportsLocalStorage
 }
