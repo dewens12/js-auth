e532e0cbb76218cf970d72cbcc340e399a98d158
Meredith Underell <m.underell@gmail.com>
2018-03-22 12:43:56 -0700

    fix: Add default onAuthenticated settings

--- src/AirMapAuth.js
+++ src/AirMapAuth.js
@@ -205,7 +205,8 @@ AirMapAuth.defaults = {
     autoLaunch: false,
     domain: 'sso.airmap.io',
     language: 'en',
-    logo: 'https://cdn.airmap.io/img/login-logo.png'
+    logo: 'https://cdn.airmap.io/img/login-logo.png',
+    onAuthenticated: (authResult) => null
 }
 
 
